1368998cf4986454cb6fa07ff7eb46076cab8142
Tanishka <tanishkakautish@gmail.com>
2025-07-02 11:30:43 +0530

    two july

---
+++ twojuly.java
@@ -0,0 +1,55 @@
+//import java.util.Scanner;
+//
+////calculater
+//public class twojuly {
+//   public void add(int a,int b){
+//      int result=a+b;
+//       System.out.println(result);
+//
+//  }
+//  public void sub(int a,int b){
+//      int result=a-b;
+//      System.out.println(result);
+//
+//  }
+//    public void mul(int a,int b){
+//        int result=a*b;
+//        System.out.println(result);
+//
+//    }
+//    public void div(int a,int b){
+//        int result=a/b;
+//        System.out.println(result);
+//
+//    }
+//    public static void main(String[]args){
+//        System.out.println("Tanishka");
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter first number: ");
+//        int x = sc.nextInt();
+//        System.out.print("Enter second number: ");
+//        int y = sc.nextInt();
+//        twojuly m=new twojuly();
+//        m.add(x,y);
+//        m.sub(x,y);
+//        m.mul(x,y);
+//       m.div(x,y);
+//    }
+//}
+public class twojuly {
+    public static void main(String[] args){
+        double a=2.0;
+        double b=3.0;
+        double res=a+b;
+        System.out.println(res);
+        float r=2.0f;
+        float t=3.0f;
+        float res1=r+t;
+        System.out.println(res1);
+        long x=21213111131l;
+        long y=4876545432l;
+        long res2=x+y;
+        System.out.println(res2);
+
+    }
+}
